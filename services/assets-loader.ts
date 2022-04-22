92b6b62f564eebfaf1034183edd02f79ab41b9e9
f-mon <federico.monaldi@gmail.com>
2022-04-22 09:37:33 +0200

    1 file modified

--- services/assets-loader.ts
+++ services/assets-loader.ts
@@ -1,20 +1,35 @@
 export class AssetsLoader {
   tileMap: HTMLImageElement;
+  private _loaded: boolean = false;
 
   constructor() {}
 
   initialize(): Promise<any> {
+    return this.imageLoad(
+      'https://raw.githubusercontent.com/f-mon/hex-board-canvas/master/images/tileMap.png'
+    ).then((img) => {
+      this.tileMap = img;
+      this._loaded = true;
+    });
+  }
+
+  get loaded(): boolean {
+    return this.loaded;
+  }
+
+  imageLoad(imgSrc: string): Promise<HTMLImageElement> {
     return new Promise((resolve, reject) => {
-      this.tileMap = new Image(); // Create new img element
-      this.tileMap.addEventListener(
+      console.log('create image');
+      const img = new Image(); // Create new img element
+      img.addEventListener(
         'load',
         () => {
-          resolve(true);
+          console.log('loaded');
+          resolve(img);
         },
         false
       );
-      this.tileMap.src =
-        'https://raw.githubusercontent.com/f-mon/hex-board-canvas/master/images/tileMap.png';
+      img.src = imgSrc;
     });
   }
 }
